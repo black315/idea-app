d43e0eca2e9514c631defb507cb080f365bd9927
black <ryo.0301.1999@gmail.com>
2019-01-15 23:12:03 +0900

    construct base

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,5 +1,4 @@
 class CalendarController < ApplicationController
-  protect_from_forgery except: :create
   def index
     @reservation = Reservation.all
   end
@@ -9,6 +8,5 @@ class CalendarController < ApplicationController
   def create
     @reservation = Reservation.new(params.require(:reservation).permit(:name, :date, :time, :course))
     @reservation.save
-    redirect_to calendar_create_path
   end
 end
